4f30ff8566631210864090bbb5d21b685a70109d
Cesar Augusto <cesarsalesgomes@gmail.com>
2020-09-04 10:38:47 -0300

    Adicionado configuração de conexão com banco de dados via Typeorm + Postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    // Variáveis de ambiente
+    ConfigModule.forRoot({ load: [databaseConfig] }),
+    // Conexão com Postgres
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get('database'),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/database.ts
@@ -0,0 +1,38 @@
+import { registerAs } from '@nestjs/config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export const typeormConfig = (migrations?: (string)[], migrationsDir?: string): PostgresConnectionOptions => {
+  const {
+    NODE_ENV, DATABASE_USER, DATABASE_PASS, DATABASE_DB, DATABASE_HOST, DATABASE_DB_TEST
+  } = process.env;
+
+  // Postgres
+  const type = 'postgres';
+  // User
+  const username = DATABASE_USER;
+  // Password
+  const password = DATABASE_PASS;
+  // Database
+  const database = NODE_ENV === 'test' ? DATABASE_DB_TEST : DATABASE_DB;
+  // Host
+  const host = DATABASE_HOST;
+
+  // Entities
+  const entities = [];
+
+  return {
+    type,
+    username,
+    password,
+    database,
+    host,
+    entities,
+    synchronize: false,
+    migrations,
+    cli: {
+      migrationsDir
+    }
+  };
+};
+
+export default registerAs('database', typeormConfig);
